1fa34a4475eaa472d4f55d4670c732b4d5adc458
Gila.Hofmann <Gila.Hofmann@HN-GILA>
2022-02-03 16:25:34 +0100

    added testdata to shoppinglist_master

---
+++ src/app/shoppinglist-master.ts
@@ -0,0 +1,31 @@
+export const shoppinglistMaster = [
+	{
+		"title": "TEST_Backpack",
+		"url": "https://www.pinqponq.com/de/collections/sale/products/blok-large-vivid-monochrome",
+		"price": 45.55,
+        "category": "needed",
+        "purchased": false
+	},
+    {
+		"title": "TEST_Shoe",
+		"url": "https://www.alohas.io/products/habibi-black-leather?variant=32485317247054",
+		"price": 1045.55,
+        "category": "nice",
+        "purchased": false
+	},
+    {
+        "title": "TEST_Boot",
+		"url": "https://www.drmartens.com/de/de/p/21045001",
+		"price": 90.55,
+        "category": "needed",
+        "purchased": false
+    },
+    {
+        "title": "TEST_Bag",
+		"url": "https://www.esthe.co.uk/product/beige-crossbody-bag/",
+		"price": 19,
+        "category": "nice",
+        "purchased": true
+    }
+    
+]
