d14d473370d6e3bba0908fa221dc92d30dda83dc
Dennis H <71262926+mnkyjs@users.noreply.github.com>
2022-02-23 15:36:57 +0100

    refactor(app): implement DRY and some other best practice recommendations

--- src/app/Services/shopping.service.ts
+++ src/app/Services/shopping.service.ts
@@ -1,56 +1,60 @@
 import { Injectable } from '@angular/core';
-import { shoppinglistMaster, ShoppingItem } from '../shoppinglist-master';
+import { ShoppingItem, shoppinglistMaster } from '../shoppinglist-master';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ShoppingService {
-  apiList = [...shoppinglistMaster];
+    apiList = [...shoppinglistMaster];
 
-  categories = this.getUniqueCategories();
+    categories = this.getUniqueCategories();
 
-  generateNeedList(purchased: boolean, category?: string) {
-    let list: ShoppingItem[] = [];
-    let shoppingApiItems = this.apiList;
-    for (let i in shoppingApiItems) {
-      if (
-        shoppingApiItems[i].category === category &&
-        shoppingApiItems[i].purchased === purchased
-      ) {
-        list.push(shoppingApiItems[i]);
-      }
+    generateNeedList(purchased: boolean, category?: string) {
+        let list: ShoppingItem[] = [];
+        let shoppingApiItems = this.apiList;
+        for (let i in shoppingApiItems) {
+            if (
+                shoppingApiItems[i].category === category &&
+                shoppingApiItems[i].purchased === purchased
+            ) {
+                list.push(shoppingApiItems[i]);
+            }
+        }
+        return list;
     }
-    return list;
-  }
 
-  getPurchasedList() {
-    let list: ShoppingItem[] = [];
-    let shoppingApiItems = this.apiList;
-    for (let i in shoppingApiItems) {
-      if (shoppingApiItems[i].purchased) {
-        list.push(shoppingApiItems[i]);
-      }
+    getPurchasedList() {
+        let list: ShoppingItem[] = [];
+        let shoppingApiItems = this.apiList;
+        for (let i in shoppingApiItems) {
+            if (shoppingApiItems[i].purchased) {
+                list.push(shoppingApiItems[i]);
+            }
+        }
+        return list;
     }
-    return list;
-  }
 
-  getList(purchased: boolean, category?: string) {
-    if (purchased) {
-      return this.getPurchasedList();
+    getList(purchased: boolean, category?: string) {
+        if (purchased) {
+            return this.getPurchasedList();
+        }
+        return this.generateNeedList(purchased, category);
     }
-    return this.generateNeedList(purchased, category);
-  }
 
-  addItem(item: ShoppingItem) {
-    this.apiList = [...this.apiList, item];
-  }
+    addItem(item: ShoppingItem) {
+        this.apiList = [...this.apiList, item];
+    }
 
-  editItem(shoppingItem: ShoppingItem) {
-    this.apiList = [...this.apiList.map((item: ShoppingItem) => item.title === shoppingItem.title ? shoppingItem : item)]
-  }
+    editItem(shoppingItem: ShoppingItem) {
+        this.apiList = [...this.apiList.map((item: ShoppingItem) => item.guid === shoppingItem.guid ? shoppingItem : item)];
+    }
 
-  getUniqueCategories() {
-    let unfiltered = this.apiList.map((shoppingItem) => shoppingItem.category);
-    return unfiltered.filter((value, index) => unfiltered.indexOf(value) === index);
-  }
+    deleteItem(shoppingItemGuid: string) {
+        this.apiList = [...this.apiList.filter((item: ShoppingItem) => item.guid !== shoppingItemGuid)];
+    }
+
+    getUniqueCategories() {
+        let unfiltered = this.apiList.map((shoppingItem) => shoppingItem.category);
+        return unfiltered.filter((value, index) => unfiltered.indexOf(value) === index);
+    }
 }
---
+++ src/app/shopping-item-list/shopping-item-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ShoppingItem } from '../shoppinglist-master';
+
+@Component({
+    selector: 'app-shopping-item-list',
+    templateUrl: './shopping-item-list.component.html',
+    styleUrls: ['./shopping-item-list.component.css'],
+})
+export class ShoppingItemListComponent {
+    isPurchasedThVisible = true;
+
+    @Input()
+    listTitle = '';
+
+    @Output()
+    purchased: EventEmitter<ShoppingItem> = new EventEmitter();
+
+    @Output()
+    shoppingItem: EventEmitter<ShoppingItem> = new EventEmitter<ShoppingItem>();
+
+    @Output()
+    deleteShoppingItem: EventEmitter<string> = new EventEmitter<string>();
+
+    _shoppingList: ShoppingItem[] = [];
+
+    get shoppingList(): ShoppingItem[] {
+        return this._shoppingList;
+    }
+
+    @Input() set shoppingList(shoppingItems: ShoppingItem[]) {
+        this.isPurchasedThVisible = shoppingItems.some((shoppingItem: ShoppingItem) => !shoppingItem.purchased);
+        this._shoppingList = [...shoppingItems];
+    }
+
+    onPurchased(shoppingItem: ShoppingItem) {
+        this.purchased.emit(shoppingItem);
+    }
+
+    onEdit(shoppingItem: ShoppingItem) {
+        this.shoppingItem.emit(shoppingItem);
+    }
+
+    onDelete(shoppingItem: ShoppingItem) {
+        this.deleteShoppingItem.emit(shoppingItem.guid);
+    }
+}
