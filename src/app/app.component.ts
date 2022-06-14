86c4790014cbe04000ae92b84594648d47cdd6b2
Gila.Hofmann <gila.hofmann@holistic-net.de>
2022-06-14 16:42:00 +0200

    edited readme, added alert to save-button-event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,6 +80,7 @@ export class AppComponent implements OnInit {
 
   onSaveList() {
       this.shoppingService.saveListToStorage();
+      window.alert("Entries Successfully Saved to this Browser Storage!");
   }
 
   onRestoreFromLocalStorage() {
